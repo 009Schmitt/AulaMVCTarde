734277bbb84226391c79014ae7109f36b626d9d4
009Schmitt <34982675+009Schmitt@users.noreply.github.com>
2021-08-23 17:26:18 -0300

    InitialCommit

---
+++ View/Form1.cs
@@ -0,0 +1,30 @@
+﻿using AULAMVC01Tarde.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AULAMVC01Tarde
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(rbExec02.Checked)
+            {
+                new Exec02().Show();
+                this.Hide();
+            }
+        }
+    }
+}
